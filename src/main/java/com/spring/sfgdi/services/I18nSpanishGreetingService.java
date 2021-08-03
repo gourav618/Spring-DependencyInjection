54ec9727067f73234f7471a3af58ba4af13aef1f
Gourav Sharma <sgourav533@gmail.com>
2021-08-03 22:39:44 +0530

    Using default profile ,hence no need to add active profile in application.properties

--- src/main/java/com/spring/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/spring/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,11 +3,12 @@ package com.spring.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+//setting this bean as default profile
+@Profile({"ES","default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hola Monda -- Spanish greeting";
+        return "Hola Mundo -- Spanish greeting";
     }
 }
